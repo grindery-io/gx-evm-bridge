4042a0cad616382df566ba12ffd9a8f7d9e685ae
Joe Hu <i@sapika.ch>
2025-03-31 09:57:26 +0000

    Support bridging to TON

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { polygon, mainnet } from '@reown/appkit/networks';
 import { useEffect, useMemo, useState } from 'react';
 import { BrowserProvider, ethers } from 'ethers';
 import { CATERC20 } from './CATERC20';
+import { Address } from '@ton/core';
 
 // 1. Get projectId
 const projectId = '3412d7ac48bc6355f82302fb11dd3679';
@@ -35,6 +36,7 @@ createAppKit({
 });
 
 const gxContract = new ethers.Contract('0x8730762Cad4a27816A467fAc54e3dd1E2e9617A1', CATERC20);
+const bridgeContract = new ethers.Contract('0x9932EDE7E99b738fc577d9B8Cce3CD52E0A838Fb', ['function bridgeToTon(uint256 amount, int32 tonWorkchainId, bytes32 tonAccountId)'])
 
 function App() {
   const [message, setMessage] = useState('');
@@ -45,6 +47,14 @@ function App() {
   const gx = useMemo(() => walletProvider ? gxContract.connect(new BrowserProvider(walletProvider as any)) as ethers.Contract : null, [walletProvider]);
   const [balance, setBalance] = useState(-1n);
   const [targetChainId, setTargetChainId] = useState(2);
+  const [tonAddress, setTonAddress] = useState("");
+  const parsedTonAddress = useMemo(() => {
+    try {
+      return Address.parse(tonAddress.trim());
+    } catch (e) {
+      return null;
+    }
+  }, [tonAddress]);
   useEffect(() => {
     setBalance(-1n);
     if (!gx || !address) {
@@ -80,32 +90,88 @@ function App() {
       }
     );
   }
+  function submitBridgeToTon() {
+    if (!parsedTonAddress) {
+      return;
+    }
+    setMessage('Processing...');
+    (async () => {
+      if (!isConnected || !address || !gx) throw Error('User disconnected');
+      const signer = await new BrowserProvider(walletProvider as any).getSigner();
+      const gxWithSigner = gx.attach("0xC3493D5787d4fF987d56855C64aAd60F382B5959").connect(signer) as ethers.Contract;
+      const bridgeWithSigner = bridgeContract.connect(signer) as ethers.Contract;
+      setMessage('Processing...');
+      const amountWei = ethers.parseEther(bridgeAmount.toString())
+      const allowance = await gxWithSigner.allowance(address, bridgeWithSigner.getAddress());
+      if (allowance < amountWei) {
+        setMessage('Processing (approval tx)...');
+        await gxWithSigner.approve(bridgeWithSigner.getAddress(), amountWei).then(x => x.wait());
+      }
+      setMessage('Processing (bridge tx)...');
+      await bridgeWithSigner.bridgeToTon(amountWei, BigInt(parsedTonAddress.workChain), ethers.hexlify(parsedTonAddress.hash));
+      setMessage('Transaction submitted, you should receive fund in a few minutes')
+    })().catch(
+      (e) => {
+        console.error(e);
+        setMessage(e.toString());
+      }
+    );
+  }
   return (
-    <>
-      <p>
-        <w3m-button />
-        <w3m-network-button />
-      </p>
-      <p>Bridge amount</p>
-      <input
-        type="text"
-        value={bridgeAmount.toString()}
-        inputMode='numeric'
-        onChange={(e) => setBridgeAmount(parseInt(e.currentTarget.value, 10) || bridgeAmount)}
-      />
-      {balance >= 0 && <p>GX balance: {ethers.formatEther(balance)}</p>}
-      <p>
+    <main>
+      <div className='container'>
+        <p>
+          <w3m-button />
+          <w3m-network-button />
+        </p>
+        <p>{message}</p>
+        <p>Bridge amount</p>
         <input
-          type="button"
-          value={`Bridge to ${targetChainId === 2 ? "Ethereum" : "Polygon"}`}
-          onClick={submit}
-          disabled={
-            !isConnected || bridgeAmount <= 0 || balance < 0 || ethers.parseEther(bridgeAmount.toString()) > balance
-          }
+          type="text"
+          value={bridgeAmount.toString()}
+          inputMode='numeric'
+          onChange={(e) => setBridgeAmount(parseInt(e.currentTarget.value, 10) || bridgeAmount)}
         />
-      </p>
-      <p>{message}</p>
-    </>
+        {balance >= 0 && <p>GX balance: {ethers.formatEther(balance)}</p>}
+        <p>
+          <input
+            type="button"
+            value={`Bridge to ${targetChainId === 2 ? "Ethereum" : "Polygon"}`}
+            onClick={submit}
+            disabled={
+              !isConnected || bridgeAmount <= 0 || balance < 0 || ethers.parseEther(bridgeAmount.toString()) > balance
+            }
+          />
+        </p>
+        {network.chainId === 137 && <div>
+          <p>Bridge amount</p>
+          <input
+            type="text"
+            value={bridgeAmount.toString()}
+            inputMode='numeric'
+            onChange={(e) => setBridgeAmount(parseInt(e.currentTarget.value, 10) || bridgeAmount)}
+          />
+          {balance >= 0 && <p>GX balance: {ethers.formatEther(balance)}</p>}
+          <p>TON address</p>
+          <input
+            type="text"
+            value={tonAddress}
+            inputMode='numeric'
+            onChange={(e) => setTonAddress(e.currentTarget.value)}
+          />
+          <p>
+            <input
+              type="button"
+              value={`Bridge to TON`}
+              onClick={submitBridgeToTon}
+              disabled={
+                !parsedTonAddress || !isConnected || bridgeAmount <= 0 || balance < 0 // || ethers.parseEther(bridgeAmount.toString()) > balance
+              }
+            />
+          </p>
+        </div>}
+      </div>
+    </main>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import '@picocss/pico'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
